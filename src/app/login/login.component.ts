70684577d48dd29d6fb94ab2380af80c2c84fcdc
stillonwarpath <fr.betancourt@live.com>
2022-08-28 16:33:20 -0400

    WIP Sign Up

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +14,8 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router,
   ) { 
     this.form = this.fb.group({
       email: ['fr.betancourt@live.com', [Validators.required, Validators.email]],
@@ -22,8 +26,28 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  login() {
+  async login() {
     console.log(this.form.value);
+    const { session, error } = await this.auth.login(this.form.value);
+    console.log(error);
+    if (error) {
+
+    } else {
+      this.router.navigateByUrl('/app', { replaceUrl: true })
+    }
+  }
+
+  async register() {
+    console.log(this.form.value);
+    const { session, error, user } = await this.auth.createAccount(this.form.value);
+    console.log(error);
+    if (error) {
+
+    } else {
+      console.log(session);
+      console.log(user);
+      this.router.navigateByUrl('/app', { replaceUrl: true })
+    }
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: 'https://hfrbgnlberatayiffuvv.supabase.co',
+  supabasekey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmcmJnbmxiZXJhdGF5aWZmdXZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjE3MDkzNzEsImV4cCI6MTk3NzI4NTM3MX0.kqFPb_zHhZed08SzxtnLWXj2JlH9rsYZn1HnEr8ioHw',
 };
 
 /*
