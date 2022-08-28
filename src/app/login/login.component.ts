761d390840401168e6fd4c78d6eb7f43489f841b
stillonwarpath <fr.betancourt@live.com>
2022-08-27 21:56:15 -0400

    Added tailwind. Created login form.

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) { 
+    this.form = this.fb.group({
+      email: ['fr.betancourt@live.com', [Validators.required, Validators.email]],
+      password: ['123456', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    console.log(this.form.value);
+  }
+
+}
