df505aa3774de05187b03942db6aab697dc494dc
stillonwarpath <fr.betancourt@live.com>
2022-08-28 16:49:10 -0400

    Added supabase env to environment prod file.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://hfrbgnlberatayiffuvv.supabase.co',
+  supabasekey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmcmJnbmxiZXJhdGF5aWZmdXZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjE3MDkzNzEsImV4cCI6MTk3NzI4NTM3MX0.kqFPb_zHhZed08SzxtnLWXj2JlH9rsYZn1HnEr8ioHw',
 };
