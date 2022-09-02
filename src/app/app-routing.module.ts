deaf0af8b66bf2fdb6c5d7fb6747f4da23f98e84
stillonwarpath <fr.betancourt@live.com>
2022-09-01 21:23:43 -0400

    Implemented a guard to prevent unauthenticated user to access app.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './inside/home/home.component';
 import { LoginComponent } from './login/login.component';
 
@@ -11,6 +12,7 @@ const routes: Routes = [
   {
     path: 'app',
     component: HomeComponent,
+    canActivate: [AuthGuard]
   }
 ];
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { createClient, SupabaseClient, User } from '@supabase/supabase-js';
+import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -8,12 +9,27 @@ import { environment } from 'src/environments/environment';
 export class AuthService {
 
   private supabase: SupabaseClient;
+  private currentUser: BehaviorSubject<boolean | User | any> = new BehaviorSubject(null);
 
   constructor() { 
     this.supabase = createClient(environment.supabaseUrl, environment.supabasekey);
+
+    // Fallback
+    const user = this.supabase.auth.user();
+    if (user) {
+      this.currentUser.next(user);
+    } else {
+      this.currentUser.next(false);
+    }
+
     this.supabase.auth.onAuthStateChange((event, session) => {
       console.log('auth changed:', event);
       console.log('auth changed session:', session);
+      if (session) {
+        this.currentUser.next(session.user);
+      } else {
+        this.currentUser.next(false);
+      }
     });
   }
 
@@ -23,6 +39,10 @@ export class AuthService {
 
   login({ email, password }: {email: string, password: string}) {
     return this.supabase.auth.signIn({email, password});
+  }
+
+  getCurrentUser() {
+    return this.currentUser.asObservable();
   }
 
 }
